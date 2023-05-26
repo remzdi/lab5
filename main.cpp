ef9eab2064325890b1bebede40c375c123e74a3f
remzdi <116484641+remzdi@users.noreply.github.com>
2023-05-26 07:29:04 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,32 @@
 #include "functions.h"
+
 int main() {
     int n, m;
     getInput(n, m);
 
-    TDate *dates = new TDate[n];
-    generateDates(dates, n);
-    printDates(dates, n);
+    TTriad** triads = new TTriad*[n + m];
+    TTriad** dates = triads;
+    TTriad** times = triads + n;
 
-    TTime *times = new TTime[m];
-    generateTimes(times, m);
+    GenerateDates(dates, n);
+    GenerateTimes(times, m);
+
+    cout << endl;
+    cout << "Before processing:" << endl;
+    printDates(dates, n);
     printTimes(times, m);
+    cout << endl;
 
     printGoodDates(dates, n);
+    cout << endl;
+
     printDecreasedDates(dates, n);
+    cout << endl;
 
     increaseTimes(times, m);
+    cout << endl;
+
+    deleteTriads(triads, n + m);
 
     return 0;
 }
